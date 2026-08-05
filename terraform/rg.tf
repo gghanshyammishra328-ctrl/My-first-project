9341dfc1c39674c5297d3b5e4d9851c0d76e5eb7
ghanshyame <gghanshyammishra328@gmail.com>
2026-08-06 00:45:08 +0530

    new terraform

---
+++ terraform/rg.tf
@@ -0,0 +1,13 @@
+ 
+ variable "resource_groups" {
+   
+ }
+  
+
+resource "azurerm_resource_group" "rg" {
+  for_each = var.resource_groups
+
+  name     = each.value.name
+  location = each.value.location
+}
+
